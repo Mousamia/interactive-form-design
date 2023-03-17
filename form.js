e6a96eb7e351b1602e57d476d998b40a87c206ef
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-17 23:59:46 +0600

    kisu

--- form.js
+++ form.js
@@ -1,25 +1,24 @@
 // id function
-const elements = (id) =>  {
-    return document.getElementById(id);
-}
+const ids = (id) => document.getElementById(id);
 
-const signUp = elements('signup');
-const userName = elements('username');
-const password = elements('password');
+let classes = (className) => document.getElementsByClassName(className);
 
 
-const errMsg = document.getElementsByClassName("error");
-
-console.log(errMsg);
-
-
-console.log(signUp);
-console.log(userName);
-console.log(password);
+const form = ids('form');
+let errorMsg = classes("error");
+console.log(errorMsg[0]);
+console.log(errorMsg[1]);
 
 
 
-console.log(signUp);
-// signUp.addEventListener('submit', function(){
 
-// })
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userName = ids('username').value;
+    console.log(userName);
+    if(userName == ""){
+        errorMsg[0].innertext = ` <span> username can not be empty</span>`
+        errorMsg[0].classList.add = "block";
+    }
+
+})
